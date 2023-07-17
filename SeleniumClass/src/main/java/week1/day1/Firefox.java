7705e19ee078a07774ace39300fe9649ea3ef071
Bismi Benny <bismibenny13@gmail.com>
2023-07-17 21:15:16 +0200

    commit

---
+++ SeleniumClass/src/main/java/week1/day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 100.2f;   
+		String name ="Firefox";
+		boolean isVisible=true;
+		int year=1998;
+		char logo='m'; 
+		System.out.println("Browser name is :" +name);
+		System.out.println("Browser version is :" +version);
+		System.out.println("Browser version is visible :" +isVisible);
+		System.out.println("Browser release year :" +year);
+		System.out.println("The logo is :" +logo);
+
+	}
+//
+}
+//
